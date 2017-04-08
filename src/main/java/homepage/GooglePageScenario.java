7ef1c04a203b5d038171e8e356c8dada4d151bb2
ruslan.bilyi <beliysacred@gmail.com>
2017-04-08 17:28:04 +0300

    removed optional

--- src/main/java/homepage/GooglePageScenario.java
+++ src/main/java/homepage/GooglePageScenario.java
@@ -3,8 +3,6 @@ package homepage;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 
 /**
  * Created by Sacred on 08.04.2017.
