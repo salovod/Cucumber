22cb8ecf1b0533af2b725b06e3422e46fbd845ce
ruslan.bilyi <beliysacred@gmail.com>
2017-04-15 20:43:33 +0300

    removed optional

--- src/test/java/basic/RunCukesTest.java
+++ src/test/java/basic/RunCukesTest.java
@@ -1,16 +1,9 @@
 package basic;
 
-import com.codeborne.selenide.Configuration;
 import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
-import io.github.bonigarcia.wdm.ChromeDriverManager;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.runner.RunWith;
-
-import static com.codeborne.selenide.Selenide.close;
-import static com.codeborne.selenide.Selenide.open;
 
 
 @RunWith(Cucumber.class)
@@ -21,18 +14,5 @@ import static com.codeborne.selenide.Selenide.open;
         tags = {}
 )
 public class RunCukesTest {
-
-
-    @Before
-    public void before() {
-        ChromeDriverManager.getInstance().setup();
-        Configuration.browser = "chrome";
-        open("http://google.com");
-    }
-
-    @After
-    public void after() {
-        close();
-    }
 
 }
